2bff74d48a5c05ee272f58d52d5dd2ad722cf1b9
dev <dev@A21PC11>
2024-11-16 12:11:33 +0200

    Add project files.

---
+++ plosh.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+//class Plosh {
+//public:
+//    static int count;
+//    static void triangleS(int a, int h) {
+//        cout << (a * h) / 2;
+//        cout << endl;
+//        addCount();
+//    }
+//    static void pram(int a, int b) {
+//        cout << a * b;
+//        cout << endl;
+//        addCount();
+//    }
+//    static void addCount() {
+//        count++;
+//    }
+//};
+//
+//int Plosh::count = 0;
+
+//class stringg {
+//    string st;
+//public:
+//    stringg(): st("qwertyui") {}
+//    stringg(string d): st(d){}
+//    stringg(const stringg& other) {
+//        this->st = other.st;
+//    }
+//    void show() {
+//        cout << st << endl;
+//    }
+//};
+
+class arr {
+    int* arra;
+public:
+    arr(int size) {
+
+    }
+};
+
+int main()
+{
+
+
+    /*stringg st1;
+    stringg st2("fdfds");
+    stringg st3(st2);
+    st1.show();
+    st2.show();
+    st3.show();*/
+
+    /*Plosh pl1;
+    pl1.triangleS(2, 3);
+    Plosh::pram(2, 4);
+    cout << Plosh::count;*/
+}
